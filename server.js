245f2b2e82a5e694f52015053cd1eb4a107c0217
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-09-03 20:28:37 -0400

    test ;

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import path from 'path';
 import favicon from 'serve-favicon';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 dotenv.config({ path: join(__dirname, '.env') });
 
