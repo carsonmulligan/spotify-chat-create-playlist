7ce9c22af76ce3fda743cafab47d8f691e2aec76
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-09-15 01:13:26 -0400

    f

---
+++ schema.sql
@@ -0,0 +1,8 @@
+-- schema.sql
+CREATE TABLE IF NOT EXISTS users (
+  user_id VARCHAR(255) PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  first_name VARCHAR(255),
+  playlist_count INTEGER NOT NULL DEFAULT 0,
+  is_subscribed BOOLEAN NOT NULL DEFAULT FALSE
+);
