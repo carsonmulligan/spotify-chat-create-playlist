9a7dff74541b090dc0701a4cb1f656534c2c978a
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-09-15 01:31:25 -0400

    add sqlite3

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
 
 app.get('/login', spotifyLogin);
