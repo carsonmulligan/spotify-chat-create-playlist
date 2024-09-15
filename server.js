f89055c806b41866fb08f498c13811f5a2cad134
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-09-15 12:27:33 -0400

    requiressl

--- server.js
+++ server.js
@@ -50,9 +50,11 @@ app.use((req, res, next) => {
 // PostgreSQL database initialization
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: {
+    rejectUnauthorized: false,
+    sslmode: 'require'
+  }
 });
-
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
   process.exit(-1);
